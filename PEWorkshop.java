a94c6c59e1775a8d3d2adc218c31c2f7a4dfeb24
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2024-01-28 21:53:19 +0530

    MMap version

--- PEWorkshop.java
+++ PEWorkshop.java
@@ -16,17 +16,37 @@
 
 package dev.morling.onebrc;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.*;
+import sun.misc.Unsafe;
+
+import java.io.*;
+import java.lang.foreign.Arena;
+import java.lang.reflect.Field;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CalculateAverage_PEWorkshop {
 
     /**
-     * Most naive implementation. Simply streaming lines using BuffredReader and parsing them
+     * Slightly improved version. MMapping the file and then manually finding lines and parsing
      */
     private static final String FILE_NAME = "./measurements.txt";
+
+    private static Unsafe initUnsafe() {
+        try {
+            final Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            return (Unsafe) theUnsafe.get(Unsafe.class);
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private final static Unsafe UNSAFE = initUnsafe();
 
     private record Row(float minTemp, float maxTemp, int count, float sum) {
         Row update(float temperature) {
@@ -47,16 +67,39 @@ public class CalculateAverage_PEWorkshop {
 
     private static final Map<String, Row> records = new HashMap<>();
 
-    public static void main(String[] args) throws FileNotFoundException {
+    private static void processLine(byte[] barray, int size, int semiColonIndex) {
+        String name = new String(barray, 0, semiColonIndex);
+        String temperatureStr = new String(barray, semiColonIndex + 1, size - semiColonIndex - 1);
+        float temperature = Float.parseFloat(temperatureStr);
+        records.compute(name, (k, v) -> v == null ? Row.create(temperature) : v.update(temperature));
+    }
+
+    private static void readFile(long startAddress, long endAddress) {
+        long currentOffset = startAddress;
+        byte[] barray = new byte[512];
+        int barrayOffset = 0;
+        while (currentOffset < endAddress) {
+            byte b;
+            int semiColonIndex = -1;
+            while ((b = UNSAFE.getByte(currentOffset++)) != '\n') {
+                if (b == ';') {
+                    semiColonIndex = barrayOffset;
+                }
+                barray[barrayOffset++] = b;
+            }
+            processLine(barray, barrayOffset, semiColonIndex);
+            barrayOffset = 0;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
         String filename = args.length > 0 ? args[0] : FILE_NAME;
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        br.lines().forEach(l -> {
-            int semiColonIndex = l.indexOf(";");
-            String locationName = l.substring(0, semiColonIndex);
-            String temperatureValStr = l.substring(semiColonIndex + 1);
-            float temperature = Float.parseFloat(temperatureValStr);
-            records.compute(locationName, (k, v) -> v == null ? Row.create(temperature) : v.update(temperature));
-        });
+        FileChannel fc = FileChannel.open(Paths.get(filename), StandardOpenOption.READ);
+        final long fileSize = fc.size();
+        final long startAddress = fc.map(FileChannel.MapMode.READ_ONLY, 0, fileSize, Arena.global()).address();
+        final long endAddress = startAddress + fileSize;
+        readFile(startAddress, endAddress);
         System.out.println(new TreeMap<>(records));
     }
 }
